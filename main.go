fbcbeb37ef2cd882eb183c2171865978ff9fc960
stnnnghm <67979808+stnnnghm@users.noreply.github.com>
2020-12-23 15:45:57 -0700

    add readme

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/kardianos/service"
+)
+
+const serviceName = "Simple Service"
+const serviceDesc = "Simple Service Description"
+
+type program struct{}
+
+func (p program) Start(s service.Service) error {
+	fmt.Println(s.String() + " started")
+	go p.run()
+	return nil
+}
+
+func (p program) Stop(s service.Service) error {
+	fmt.Println(s.String() + " stopped")
+	return nil
+}
+
+func (p program) run() {
+	for {
+		fmt.Println("Service is running")
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	serviceConfig := &service.Config{
+		Name:        serviceName,
+		DisplayName: serviceName,
+		Description: serviceDesc,
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, serviceConfig)
+	if err != nil {
+		fmt.Println("Cannot create the service: " + err.Error())
+	}
+
+	err = s.Run()
+	if err != nil {
+		fmt.Println("Cannot start the service: " + err.Error())
+	}
+
+}
